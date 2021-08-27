0285e3d636960bc3ef906ce90ae8a0e5bff74b1d
Hassan <hassan.alirobleh@gmail.com>
2021-08-27 18:57:07 +0200

    update tdd

--- test/strategie.test.ts
+++ test/strategie.test.ts
@@ -1,19 +1,19 @@
 import { App } from './../src/classes/App';
 
 describe('Stratégie test', () => {
-    it("'should be return Cancane et Je vole !!!", () => {
+    it("should be return Cancane et Je vole !!!", () => {
         // let app = new App();
         // let result = app.main();
         expect(App.main()).toBe("Cancane et Je vole !!!");
     });
 
-    it("'should be return Je ne sais pas voler !!!", () => {
+    it("should be return Je ne sais pas voler !!!", () => {
         // let app = new App();
         // let result = app.main();
         expect(App.main1()).toBe("Je ne sais pas voler !!!");
     });
 
-    it("'should be return Je vole avec un réaction !", () => {
+    it("should be return Je vole avec un réaction !", () => {
         // let app = new App();
         // let result = app.main();
         expect(App.main2()).toBe("Je vole avec un réaction !");
